498117a444de95994b2c578607c323f64eeffa12
Cynthia France <cindyzining@gmail.com>
2022-08-14 21:06:46 -0400

    add comments
    add comments to code

--- RideSharer/MapViewController.swift
+++ RideSharer/MapViewController.swift
@@ -17,20 +17,27 @@ class MapViewController: UIViewController {
         checkLocationServices()
     }
     
+    //check if location services are enabled
     func checkLocationServices() {
+        //if enabled, check for authorization status
         if CLLocationManager.locationServicesEnabled() {
             checkAuthorizationStatus()
+        //if not enabled, display an alert saying so
         } else {
             createAlert(title: "Unable to Display Location", message: "enable location services")
         }
     }
     
+    //check for authorization status
     func checkAuthorizationStatus() {
         switch locationManager.authorizationStatus {
+            //if allowed to access location, display the current location
             case .authorizedWhenInUse, .authorizedAlways:
                 mapView.showsUserLocation = true
+            //if not allowed, display an alert saying so
             case .denied, .restricted:
                 createAlert(title: "Unable to Display Location", message: "allow app to access your location to enable map feature")
+            //if unknown or not set, ask user to select authorization
             case .notDetermined:
                 locationManager.requestWhenInUseAuthorization()
                 mapView.showsUserLocation = true
@@ -39,6 +46,7 @@ class MapViewController: UIViewController {
         }
     }
     
+    //create an alert with the specified title and message
     func createAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
             // add ok button
